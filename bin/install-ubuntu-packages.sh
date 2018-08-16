c383aeb5cfbd43711113ace296e10c6bb78893fc
Mattias Frinnström <mattias@frinnstrom.se>
2018-08-16 17:22:03 +0200

    Updated Ubuntu install script with new packages

--- bin/install-ubuntu-packages.sh
+++ bin/install-ubuntu-packages.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-sudo apt install curl git zsh i3 compton rofi dunst xautolock
+echo "Installing packages..."
+sudo apt install xserver-xorg-input-evdev stow curl zsh i3 compton rofi dunst xautolock python3-pip gawk
+
+echo "Installing zplug..."
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
+echo "Installing vim-plug..."
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
